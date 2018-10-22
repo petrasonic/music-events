cb6f43fd842c7c1404abd3786a27e3c836dfd520
Dave <davidpetrasovic@gmail.com>
2018-10-22 18:29:32 -0400

    Moved App.js to it's own folder and displayed data in list of divs

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
   state = {
-    music: [],
+    musicEvents: [],
   }
 
   getData = () => {
     axios.get('/api/get-events').then((res) => {
       console.log(res);
-      this.setState({music: JSON.stringify(res.data)})
+      this.setState({musicEvents: res.data})
     });
   }
   render() {
+    const { musicEvents } = this.state;
+    console.log(musicEvents);
     return (
       <div className="App">
+        {musicEvents.map(musicEvent => (
+          <div className="event">{musicEvent['ON SALE']}</div>
+        ))}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -30,8 +32,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        <button onClick={this.getData}>Get Data</button>
-        <pre>{this.state.music}</pre>
+          <button onClick={this.getData}>Get Data</button>
         </header>
       </div>
     );
